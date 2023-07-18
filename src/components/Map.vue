fa3c3b5bede975a80ae1482def5d8163d4ecfc0c
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2023-07-18 12:16:34 +0545

    fix: update map location

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="map">
     <iframe
-      src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3533.320103506886!2d85.31674607617381!3d27.676499726850082!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjfCsDQwJzM1LjQiTiA4NcKwMTknMDkuNiJF!5e0!3m2!1sen!2snp!4v1689583033424!5m2!1sen!2snp"
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d220.8326112675866!2d85.31931606552774!3d27.67644788522575!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb195d0c6e99c5%3A0xca0927e6daf122a4!2sLoop%20Studio%20Cafe!5e0!3m2!1sen!2snp!4v1689661832102!5m2!1sen!2snp"
       width="100%"
       height="450"
       frameborder="0"
